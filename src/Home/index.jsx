6ab28c299c12bf9f12f249df9646acdd5d336c0b
Eva Janoskova <janoskova.evca@gmail.com>
2021-05-28 10:59:23 +0200

    JourneyPicker added

--- src/Home/index.jsx
+++ src/Home/index.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
+import JourneyPicker from '../JourneyPicker/index';
 
 const Home = () => {
   return (
     <>
+      <JourneyPicker />
       <h2>Domovska stranka</h2>
     </>
   );
