a9f47f817461485b9f44b415f1bef8def3675ffe
Eva Janoskova <janoskova.evca@gmail.com>
2021-05-28 10:18:32 +0200

    react router

---
+++ src/Reservation/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Reservation = () => {
+  return (
+    <>
+      <h2>Rezervace</h2>
+    </>
+  );
+};
+
+export default Reservation;
--- src/index.jsx
+++ src/index.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import { render } from 'react-dom';
 import Footer from './Footer/index';
 import Header from './Header/index';
+import Home from './Home/index';
+import Reservation from './Reservation/index';
 
 import {
   BrowserRouter as Router,
@@ -14,10 +16,18 @@ import {
 import './style.css';
 
 const App = () => (
-  <>
+  <Router>
     <Header />
+    <Switch>
+      <Route exact path="/">
+        <Home />
+      </Route>
+      <Route exact path="/reservation">
+        <Reservation />
+      </Route>
+    </Switch>
     <Footer />
-  </>
+  </Router>
 );
 
 render(<App />, document.querySelector('#app'));
